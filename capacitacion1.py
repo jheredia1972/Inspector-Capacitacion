854736e6482142a70f483b8c5d6da3e618c0b4de
Jorge Alfredo Heredia Vizcaino <Jorge.Heredia@tigo.com.co>
2020-11-06 09:34:11 -0500

    Probando nuevamente el brahching

--- capacitacion1.py
+++ capacitacion1.py
@@ -1,5 +1,5 @@
 def main():
     print("Hello, world")
-    print("Esto es un cambio nuevo en la rama jheredia1972/indentaciones")
+    print("Esto es el cambio nuevo en la rama jheredia1972/indentaciones")
 if __name__ == "__main__":
     main()
